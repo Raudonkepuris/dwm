ff68ecd600bf1e4d123c352a386300942a125ebd
Raudonkepuris <skamarakas.liutauras@protonmail.com>
2025-08-01 06:16:36 +0300

    binds

--- config.h
+++ config.h
@@ -121,8 +121,6 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_equal,  setgaps,        {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  } },
 
-	{ MODKEY,                       XK_x,      movecenter,     {0} },
-
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
